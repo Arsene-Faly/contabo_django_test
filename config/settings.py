cf6a3adee71ae119a17231fb8ea052ee03db8f82
Arsene-Faly <arsenefaly1@gmail.com>
2026-09-23 13:47:59 +0300

    Mysql

--- config/settings.py
+++ config/settings.py
@@ -76,10 +76,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+# Sqlite
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Mysql
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "blog_db",
+        "USER": "arsenetsiri",
+        "PASSWORD": "tsiriarsene@**",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
